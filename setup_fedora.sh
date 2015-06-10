e4c0adea94527cda40e728ca2e510d5f5c069d2e
ZhangJingyuan <jason.jingyuan.zhang@gmail.com>
2015-06-10 15:13:08 +0800

    IP service changed

--- setup_fedora.sh
+++ setup_fedora.sh
@@ -114,7 +114,7 @@ else
         yum install net-tools -y > /dev/null
 fi
 
-PUBLICIP=`wget -q -O - http://wtfismyip.com/text`
+PUBLICIP=`wget -q -O - http://ipecho.net/plain`
 if [ "x$PUBLICIP" = "x" ]
 then
   echo "Your server's external IP address could not be detected!"
